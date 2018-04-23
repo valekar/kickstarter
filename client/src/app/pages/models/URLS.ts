68325e20afc0be2999aa16d5f675fb4ccb14d056
valekar <srinivas.valekar@gmail.com>
2018-04-24 02:27:42 +0800

    added server code

--- client/src/app/pages/models/URLS.ts
+++ client/src/app/pages/models/URLS.ts
@@ -3,7 +3,7 @@ export class URLS{
     //public static cloud_provider = "https://server-cloud-heroku.herokuapp.com";
     //public static cloud_provider = "http://cloud-env.vmfc4stcsn.eu-west-1.elasticbeanstalk.com/";
     //dev
-    public static cloud_provider = "";
+    public static cloud_provider = "http://ec2-52-214-70-14.eu-west-1.compute.amazonaws.com:8080";
     public static GET_CATEGORIES = URLS.cloud_provider +"/api/categories/";
     public static GET_SUB_CATEGORIES = URLS.cloud_provider + "/api/sub/categories/";
     public static GET_COUNTRIES = URLS.cloud_provider + "/api/countries/";
